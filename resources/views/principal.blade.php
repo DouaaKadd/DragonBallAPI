0f2456808b20459a1ae26910612bb0434e086005
Douaa <douaa.kaddar@insbaixcamp.car>
2025-10-31 14:01:03 +0100

    Laravel Cloud

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
   <meta charset="UTF-8">
-  <title>DragonBall</title>
+  <title>DragonBall Laravel Cloud</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
   <style>
